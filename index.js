bd9959fce838394550ba861eced2cff1383283d7
dwleonhardt <dwleonhardt@gmail.com>
2017-11-13 17:49:20 -0700

    add index.html

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.use(bodyParser.json());
+
+app.use(express.static('public'));
 
 app.listen(3000, console.log('listening port 3000'));
